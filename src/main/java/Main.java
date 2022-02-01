2478b15f426ec568c209479b70455b95cf2b0618
Stacy Degteva <degtich@mail.ru>
2022-02-01 15:03:05 +0400

    First commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean registered = true;
+        int percent = registered ?3:1;
+        long amount = 1000_60;
+        long bonus = amount * percent / 100 / 100;
+        long limit = 500;
+        if (bonus > limit) {
+            bonus = limit;
+        }
+        System.out.println(bonus);
+    }
+
+}
